32a16585a6c272f14488665b5bc772091ac5973b
MuhammadHunain0314 <156586251+MuhammadHunain0314@users.noreply.github.com>
2024-02-11 22:27:17 +0500

    Add files via upload

---
+++ Assignment6-2.js
@@ -0,0 +1,6 @@
+document.getElementById("div").addEventListener("mouseenter", () => {
+    document.getElementById("div").style.backgroundColor = "red";
+  });
+  document.getElementById("div").addEventListener("mouseout", () => {
+    document.getElementById("div").style.backgroundColor = "white";
+  });
